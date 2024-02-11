22c559838142bc79fff8c98eb5b0f9ab25bf6c2a
lesuuda <lesuudaljamani@gmail.com>
2024-02-11 22:49:39 +0300

    fixed different ids for reloaded objects

--- models/base_model.py
+++ models/base_model.py
@@ -18,24 +18,21 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """
-        instatiattion method for an object with its attributes
+        instantiation method for an object with its attributes
         """
-        if len(kwargs) > 0:
+        if kwargs:
             for key, value in kwargs.items():
                 if key == '__class__':
                     continue
                 if key == 'created_at' or key == 'updated_at':
                     value = datetime.fromisoformat(value)
-                    setattr(self, key, value)
+                setattr(self, key, value)
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
-    
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        models.storage.new(self)
+
 
     def __str__(self):
         """
