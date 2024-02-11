30f4b306ce3a7ca866463ffe7da1af7c0bfd791f
lesuuda <lesuudaljamani@gmail.com>
2024-02-11 14:00:09 +0300

    tests for BaseModel

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -5,11 +5,23 @@ Tests for the BaseModel class
 
 
 import unittest
+import os
 from models.base_model import BaseModel
 from datetime import datetime
+from models.engine.file_storage import FileStorage
 
 class TestBaseModel(unittest.TestCase):
     """unittests for the base class"""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self) -> None:
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.exists(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
     def test_creation_of_instance(self):
         obj = BaseModel()
         obj1 = BaseModel()
@@ -40,6 +52,6 @@ class TestBaseModel(unittest.TestCase):
         self.assertIn('updated_at', obj_dict)
         self.assertIn('__class__', obj_dict)
         self.assertEqual(obj_dict['__class__'], type(obj).__name__)
-        self.assertEqual(obj_dict['created_at'], type(obj).__name__)
-        self.assertEqual(obj_dict['updated_at'], type(obj).__name__)
+        self.assertEqual(obj_dict['created_at'], obj.created_at.isoformat())
+        self.assertEqual(obj_dict['updated_at'], obj.updated_at.isoformat())
     
