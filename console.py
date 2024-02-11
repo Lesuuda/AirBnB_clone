31c55782d2e0f41c63e00847afb450a7da8f6115
lesuuda <lesuudaljamani@gmail.com>
2024-02-11 15:26:17 +0300

    added descripyion for console.py

--- console.py
+++ console.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
 """
  contains the entry point of the command interpreter
+ it enables us to perfom different tasks like creating, updating
+ showing and destroying items
+ Typical usage example:
+
+    $ ./console
+    (hbnb)
+
+    (hbnb) help
+    Documented commands (type help <topic>):
+    ========================================
+    EOF  create  help  quit
+
+    (hbnb)
+    (hbnb) quit
+    $
 """
 
 
